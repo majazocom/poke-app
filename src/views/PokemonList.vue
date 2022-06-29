1febd8bc8d3c862de941623ac8cffabcec2ea1e2
Maja Gedda <maja.gedda@zocom.se>
2022-06-29 11:01:22 +0200

    views setup

---
+++ src/views/PokemonList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script>
+import Pokemon from '@/components/Pokemon.vue';
+
+export default {
+  name: 'PokemonList',
+  components: {
+    Pokemon
+  },
+  computed: {
+    pokemons() {
+      return 
+    }
+  }
+}
+</script>
