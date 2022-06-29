9f3f7b0e399b52ee78cc1360343e6f6596a9c2bd
Maja Gedda <maja.gedda@zocom.se>
2022-06-29 11:01:38 +0200

    component fix 2

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,8 @@
+<template>
+    <article>
+        <section>
+            <h2>Name</h2>
+            <h3>Type</h3>
+        </section>
+    </article>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PokemonList from '../views/PokemonList.vue'
 
 const routes = [{
     path: '/',
     name: 'home',
-    component: HomeView
+    component: PokemonList
 }]
 
 const router = createRouter({
